245c3a2535d8619e50f7c9756cf96a6fa7b27a68
GeorqeMilev <GeorqeMilev@Gmail.com>
2014-09-14 17:48:32 +0300

    Closing inputs
    Closing inputs

--- src/blockBreaker/BlockBreaker.java
+++ src/blockBreaker/BlockBreaker.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 
 import blockBreaker.Player;
 
-public class BlockBreaker
-{
+public class BlockBreaker {
 	public static BlockBreaker game;
 	public Player player;
-	
-	public void GamePanel()
-	{
+
+	public void GamePanel() {
 		// TODO
 	}
 
-	//Get user name and level
-	public static void startGame()
-	{
+	// Get user name and level
+	public static void startGame() {
 		Scanner input = new Scanner(System.in);
 		Player player = new Player();
 		System.out.print("Write your user name:");
@@ -27,19 +24,19 @@ public class BlockBreaker
 		System.out.println("3 - hard");
 		int levelNumber = input.nextInt();
 		player.setLevel(levelNumber);
-
-	}
-	
-	public static void main(String[] args) 
-	{
-
-		//game = new BlockBreaker();
 		
+		input.close();
+	}
+
+	public static void main(String[] args) {
+
+		// game = new BlockBreaker();
+
 		Scanner input = new Scanner(System.in);
-		
+
 		startGame();
-	
+		
+		input.close();
 	}
-	
-	
+
 }
